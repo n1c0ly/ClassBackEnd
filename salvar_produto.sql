f5b2f1d918eae4b8eb133f09505832d138f5b0bd
Nicoly dos Santos Dias <santosnico048@gmail.com>
2025-03-10 12:06:15 -0300

    Update salvar_produto.sql

--- salvar_produto.sql
+++ salvar_produto.sql
@@ -1,7 +1,7 @@
 <%@page import="java.sql.Connection" %>
 <%@page import="java.sql.DriverManager" %>
-<%@page import="java.sql.*" %>
-
+<%@page import="java.sql.PreparedStatement" %>
+<%@page import="java.sql.SQLException" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
@@ -12,49 +12,47 @@
     <body>
   
         <%
-            //receber dados digitados do formulario cadpro
-
+            // Receber dados digitados do formulário cadpro
             int codigo;
             String nome, marca;
             double preco;
 
             codigo = Integer.parseInt(request.getParameter("codigo"));
-            nome = request.getParameter(("nome"));
+            nome = request.getParameter("nome"); // Removido parêntese extra
             marca = request.getParameter("marca");
             preco = Double.parseDouble(request.getParameter("preco"));
-            //Fazer a conexão com o banco de dados
 
+            // Fazer a conexão com o banco de dados
             try {
                 Connection conecta;
-                PreparedStatement st; //Esse objeto permite enviar vários
-                // comandos sql como um grupo unico para um banco de dados
-                Class.forName("com.mysql.cj.jdbc.Driver"); //este método é usado para que o servidor 
-                //de aplicação faça o registro do driver do banco
+                PreparedStatement st;
+
+                // Registrar o driver JDBC
+                Class.forName("com.mysql.cj.jdbc.Driver");
                 String url = "jdbc:mysql://localhost:3306/produto";
                 String user = "root";
                 String password = "root";
 
                 conecta = DriverManager.getConnection(url, user, password);
 
-                String sql = ("INSERT INTO produto VALUES (?, ?, ?, ?)");
+                // SQL para inserção de dados
+                String sql = "INSERT INTO produto VALUES (?, ?, ?, ?)";
                 st = conecta.prepareStatement(sql);
                 st.setInt(1, codigo);
                 st.setString(2, nome);
                 st.setString(3, marca);
                 st.setDouble(4, preco);
-                st.executeUpdate(); //executar a instrução INSERT
-                out.print("<p style= 'color: green; font-size:25px'>Produto cadastrado com sucesso...</p>");
-            } catch (Exception x) {
-                String erro=x.getMessage();
-                if (erro.contains("Duplicate entry")) {
-                out.print("<p style='color: purple; font-size25px'>Este produto está cadastrado</p>");
-            } else {
-            out.print("<p style='color: red; font-size:25px'>Mensagem de erro: " + erro + "</p>");
-            
-            
-            }
-            }
+                st.executeUpdate(); // Executa o comando SQL
 
+                out.print("<p style='color: green; font-size: 25px;'>Produto cadastrado com sucesso...</p>");
+            } catch (Exception x) {
+                String erro = x.getMessage();
+                if (erro.contains("Duplicate entry")) {
+                    out.print("<p style='color: purple; font-size: 25px;'>Este produto já está cadastrado</p>");
+                } else {
+                    out.print("<p style='color: red; font-size: 25px;'>Mensagem de erro: " + erro + "</p>");
+                }
+            }
         %>
     </body>
 </html>
